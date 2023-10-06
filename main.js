29fb80c660fa919329a09d04d422391676ff543a
FroxCC <40371432+FroxCC@users.noreply.github.com>
2023-10-06 16:13:25 -0400

    add functionality for random msg generator

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("Hello world!")
+const adjectives = ["Amazing", "Incredible", "Fantastic", "Inspiring", "Brilliant"];
+const verbs = ["achieve", "believe", "succeed", "dream", "imagine"];
+const nouns = ["happiness", "success", "fulfillment", "greatness", "wisdom"];
+
+
+function getRandomItem(arr) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+}
+
+function getRandomMsg() {
+    const randomAdjective = getRandomItem(adjectives);
+    const randomVerb = getRandomItem(verbs);
+    const randomNoun = getRandomItem(nouns);
+    return `${randomAdjective} people ${randomVerb} ${randomNoun}!`;
+}
+
+
+console.log(getRandomMsg())
